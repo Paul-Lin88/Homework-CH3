374b5e71c98e56ac05b3542ad1f0b587d1a713d7
Paul-Lin88 <t109360116@ntut.org.tw>
2022-11-15 23:12:45 +0800

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module practice1110 {
+}
---
+++ src/practice1110/Practice01.java
@@ -0,0 +1,13 @@
+package practice1110;
+
+public class Practice01 {
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+	}
+}
+/*class Car
+{
+	
+}*/
